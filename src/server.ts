a2347d830027a253d1901ff4ccf7ee1a7488e829
Joao Vanelli <vanellijoao@gmail.com>
2023-10-02 17:11:41 -0300

    validations

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,21 @@
 import fastify from 'fastify'
 import crypto from 'node:crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Transação de teste',
-      amount: 1000,
-    })
-    .returning('*')
+  const transactions = await knex('transactions')
+    .where('amount', 500)
+    .select('*')
 
-  return transaction
+  return transactions
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP server running! o/')
